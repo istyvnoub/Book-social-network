6bbdaedb5bc2a24c4e182ab8821051e529a2f08f
yvan <noubomyvan96@gmail.com>
2025-09-13 16:35:49 +0200

    feat(book): add endpoint to update book shareable status

--- book-network/src/main/java/com/yvan/book/book/BookController.java
+++ book-network/src/main/java/com/yvan/book/book/BookController.java
@@ -2,6 +2,7 @@ package com.yvan.book.book;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -66,6 +67,14 @@ public class BookController {
             Authentication connectedUser
     ) {
         return ResponseEntity.ok(service.findAllReturnedBooks(page, size, connectedUser));
+    }
+
+     @PatchMapping("/shareable/{book-id}")
+    public ResponseEntity<Integer> updateShareableStatus(
+            @PathVariable("book-id") Integer bookId,
+            Authentication connectedUser
+    ) {
+        return ResponseEntity.ok(service.updateShareableStatus(bookId, connectedUser));
     }
 
 }
