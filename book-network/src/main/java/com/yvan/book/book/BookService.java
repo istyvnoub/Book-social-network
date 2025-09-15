adef78b2fd41b4e709058208564d7a47ab96a69e
yvan <noubomyvan96@gmail.com>
2025-09-15 16:48:57 +0200

    feat(book): add borrow book functionality with permission checks

--- book-network/src/main/java/com/yvan/book/book/BookService.java
+++ book-network/src/main/java/com/yvan/book/book/BookService.java
@@ -8,6 +8,7 @@ import com.yvan.book.exception.OperationNotPermittedException;
 import com.yvan.book.file.FileStorageService;
 import com.yvan.book.history.BookTransactionHistory;
 import com.yvan.book.history.BookTransactionHistoryRepository;
+import com.yvan.book.user.User;
 
 import jakarta.persistence.EntityNotFoundException;
 
@@ -147,6 +148,36 @@ public class BookService {
         book.setArchived(!book.isArchived());
         bookRepository.save(book);
         return bookId;
+    }
+
+        public Integer borrowBook(Integer bookId, Authentication connectedUser) {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new EntityNotFoundException("No book found with ID:: " + bookId));
+        if (book.isArchived() || !book.isShareable()) {
+            throw new OperationNotPermittedException("The requested book cannot be borrowed since it is archived or not shareable");
+        }
+        User user = ((User) connectedUser.getPrincipal());
+        if (Objects.equals(book.getCreatedBy(), user.getName())) {
+            throw new OperationNotPermittedException("You cannot borrow your own book");
+        }
+        final boolean isAlreadyBorrowedByUser = transactionHistoryRepository.isAlreadyBorrowedByUser(bookId, connectedUser.getName());
+        if (isAlreadyBorrowedByUser) {
+            throw new OperationNotPermittedException("You already borrowed this book and it is still not returned or the return is not approved by the owner");
+        }
+
+        final boolean isAlreadyBorrowedByOtherUser = transactionHistoryRepository.isAlreadyBorrowed(bookId);
+        if (isAlreadyBorrowedByOtherUser) {
+            throw new OperationNotPermittedException("Te requested book is already borrowed");
+        }
+
+        BookTransactionHistory bookTransactionHistory = BookTransactionHistory.builder()
+                .user(user)
+                .book(book)
+                .returned(false)
+                .returnApproved(false)
+                .build();
+        return transactionHistoryRepository.save(bookTransactionHistory).getId();
+
     }
 
 }
